cbc64d94d3a0e4983b69e1463be66b0f8e86d352
Jose CarlosTapiador Carretero <jose.carlos.tapiador.carretero@autodesk.com>
2020-12-23 02:36:48 +0100

    Add configuration with yaml

--- corecito.py
+++ corecito.py
@@ -2,26 +2,31 @@
 import asyncio
 import time
 import logging
+import yaml
+import sys
+from os.path import exists
 import cryptocom.exchange as cro
 from cryptocom.exchange.structs import Pair
 from cryptocom.exchange.structs import PrivateTrade
 
-CRYPTOCOM_API_KEY = "MY_KEY"
-CRYPTOCOM_API_SECRET = "MY_SECRET"
-
-DEFAULT_CORE_NUMBER = 2 # Ethers
-MAX_DECIMALS_BUY = 6 # This is the limit Crypto.com has on ETH_BTC pair https://crypto.com/exchange/trade/spot/ETH_BTC
-MAX_DECIMALS_SELL = 3 # This is the limit Crypto.com has on ETH_BTC pair https://crypto.com/exchange/trade/spot/ETH_BTC
+# CRYPTOCOM_API_KEY = "MY_KEY"
+# CRYPTOCOM_API_SECRET = "MY_SECRET"
+#
+# DEFAULT_CORE_NUMBER = 2 # Ethers
+# config['max_decimals_buy'] = 6 # This is the limit Crypto.com has on ETH_BTC pair https://crypto.com/exchange/trade/spot/ETH_BTC
+# config['max_decimals_sell'] = 3 # This is the limit Crypto.com has on ETH_BTC pair https://crypto.com/exchange/trade/spot/ETH_BTC
 
 async def main():
-  safe_mode_on = False # Set to False to enable trading, True just prints to the console log what it would do
+  config = get_config()
   logger = setupLogger('logfile.log')
   exchange = cro.Exchange()
-  account = cro.Account(api_key=CRYPTOCOM_API_KEY, api_secret=CRYPTOCOM_API_SECRET)
-  core_number = DEFAULT_CORE_NUMBER
-  pair = cro.pairs.ETH_BTC
-  base_currency = 'BTC'
-  core_number_currency = 'ETH'
+  account = cro.Account(api_key=config['cryptocom_api_key'], api_secret=config['cryptocom_api_secret'])
+  core_number = config['core_number']
+  pair = eval('cro.pairs.' + config['trading_pair'])
+  base_currency = config['base_currency']
+  cro_coin_base_currency = eval('cro.coins.' + config['base_currency'])
+  core_number_currency = config['core_number_currency']
+  cro_coin_core_number_currency = eval('cro.coins.' + config['core_number_currency'])
 
   # Get BTC/ETH ticker info
   tickers = await exchange.get_tickers()
@@ -32,32 +37,32 @@ async def main():
   low = ticker.low
   logger.info(f'\nMarket {pair.name}\nbuy price: {buy_price} - sell price: {sell_price} <> low: {low} - high: {high}\n')
 
-  # EXAMPLE ticker = MarketTicker(pair=Pair(name='ETH_BTC'), 
-    # buy_price=0.027638, sell_price=0.027641, trade_price=0.027641, 
-    # time=1608466157, volume=10596.948, 
+  # EXAMPLE ticker = MarketTicker(pair=Pair(name='ETH_BTC'),
+    # buy_price=0.027638, sell_price=0.027641, trade_price=0.027641,
+    # time=1608466157, volume=10596.948,
     # high=0.028549, low=0.027439, change=-0.001)
 
   # Get my BTC balance
   balances = await account.get_balance()
-  btc_balance = balances[cro.coins.BTC]
-  btc_available = btc_balance.available
-  # EXAMPLE btc_balance:Balance(total=0.04140678, available=3.243e-05, in_orders=0.04137435, in_stake=0, coin=Coin(name='BTC'))
+  base_currency_balance = balances[cro_coin_base_currency]
+  base_currency_available = base_currency_balance.available
+  # EXAMPLE BTC_balance:Balance(total=0.04140678, available=3.243e-05, in_orders=0.04137435, in_stake=0, coin=Coin(name='BTC'))
 
   # Get my ETH balance
-  eth_balance = balances[cro.coins.ETH]
+  core_number_currency_balance = balances[cro_coin_core_number_currency]
 
-  logger.info(f'Balances\nBTC balance:{btc_balance} \nETH balance:{eth_balance}\n')
+  logger.info(f'Balances\n(Base) {base_currency} balance:{base_currency_balance} \n(Core) {core_number_currency} balance:{core_number_currency_balance}\n')
 
   ###########################
   # Core Number Adjustments #
   ###########################
-  deviated_core_number = btc_available / buy_price
+  deviated_core_number = base_currency_available / buy_price
   logger.info(f'Core number adjustments')
   logger.info(f'Core number: {core_number} {core_number_currency}')
   logger.info(f'Deviated Core number:{deviated_core_number:.6f} {core_number_currency}')
-  excess = round(deviated_core_number - core_number, MAX_DECIMALS_BUY)
+  excess = round(deviated_core_number - core_number, config['max_decimals_buy'])
   increase_percentage = excess * 100 / core_number
-  missing = round(core_number - deviated_core_number, MAX_DECIMALS_SELL)
+  missing = round(core_number - deviated_core_number, config['max_decimals_sell'])
   decrease_percentage = missing * 100 / core_number
 
   if coreNumberExploded(core_number, deviated_core_number):
@@ -65,10 +70,10 @@ async def main():
 
   elif coreNumberIncreased(core_number, deviated_core_number):
     logger.info(f'Increased {increase_percentage:.2f}% - excess of {excess:.6f} {core_number_currency} denominated in {base_currency}')
-    tx_result = round(excess * buy_price, MAX_DECIMALS_BUY)
+    tx_result = round(excess * buy_price, config['max_decimals_buy'])
     logger.info(f'\n\n>>> Selling: {tx_result:.6f} {base_currency} at {buy_price} to park an excess of {excess:.6f} {core_number_currency}\n')
     # Sell excess BTC => Market Sell for ETH_BTC is denominated in BTC!!!
-    if (not safe_mode_on):
+    if (not config['safe_mode_on']):
       await account.buy_market(pair, tx_result)
 
   elif coreNumberDecreased(core_number, deviated_core_number):
@@ -76,7 +81,7 @@ async def main():
     tx_result = missing * sell_price
     logger.info(f'\n\n>>> Buying: {tx_result:.6f} {base_currency} at {buy_price} taking {missing:.6f} {core_number_currency} from reserves\n')
     # Buy missing BTC => Sell ETH
-    if (not safe_mode_on):
+    if (not config['safe_mode_on']):
       await account.sell_market(pair, missing)
 
   elif coreNumberPlummeted(core_number, deviated_core_number):
@@ -87,10 +92,41 @@ async def main():
 
   # Update balances after adjusting to core number
   balances = await account.get_balance()
-  logger.info(f'Final BTC available:{balances[cro.coins.BTC].available} - ETH available:{balances[cro.coins.ETH].available}\n')
+  logger.info(f'Final {base_currency} available:{balances[cro_coin_base_currency].available} - {core_number_currency} available:{balances[cro_coin_core_number_currency].available}\n')
 
 
 
+
+
+def get_config():
+  config_path = "config/default_config.yaml"
+  if exists("config/user_config.yaml"):
+    config_path = "config/user_config.yaml"
+    print('Detected user configuration...')
+  else:
+    print('Loading default configuration...')
+  config_file = open(config_path)
+  data = yaml.load(config_file, Loader=yaml.FullLoader)
+  config_file.close()
+  check_config(data)
+  return data
+
+def check_config(data):
+  try:
+    eval('cro.pairs.' + data['trading_pair'])
+  except AttributeError:
+    print('Trading pair "{}" does not exist (check your config_file)'.format(data['trading_pair']))
+    sys.exit(1)
+  try:
+    eval('cro.coins.' + data['core_number_currency'])
+  except AttributeError:
+    print('Currency "{}" does not exist (check your config_file)'.format(data['core_number_currency']))
+    sys.exit(1)
+  try:
+    eval('cro.coins.' + data['base_currency'])
+  except AttributeError:
+    print('Currency "{}" does not exist (check your config_file)'.format(data['base_currency']))
+    sys.exit(1)
 
 
 def coreNumberIncreased(core_number, deviated_core_number):
@@ -113,7 +149,7 @@ def coreNumberPlummeted(core_number, deviated_core_number):
 
 def setupLogger(log_filename):
   logger = logging.getLogger('CN')
-  
+
   file_log_handler = logging.FileHandler(log_filename)
   logger.addHandler(file_log_handler)
 
