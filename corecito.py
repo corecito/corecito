f9f2de7642db73f7dd1215d45515b2d0c35e1555
Jose CarlosTapiador Carretero <jose.carlos.tapiador.carretero@autodesk.com>
2020-12-25 01:04:38 +0100

    Add more parameters to control min/max core increase/decrease percentages and looping options

--- corecito.py
+++ corecito.py
@@ -28,73 +28,79 @@ async def main():
   core_number_currency = config['core_number_currency']
   cro_coin_core_number_currency = eval('cro.coins.' + config['core_number_currency'])
 
-  # Get BTC/ETH ticker info
-  tickers = await exchange.get_tickers()
-  ticker = tickers[pair]
-  buy_price = ticker.buy_price
-  sell_price = ticker.sell_price
-  high = ticker.high
-  low = ticker.low
-  logger.info(f'\nMarket {pair.name}\nbuy price: {buy_price} - sell price: {sell_price} <> low: {low} - high: {high}\n')
+  while True:
+    # Get BTC/ETH ticker info
+    tickers = await exchange.get_tickers()
+    ticker = tickers[pair]
+    buy_price = ticker.buy_price
+    sell_price = ticker.sell_price
+    high = ticker.high
+    low = ticker.low
+    logger.info(f'\nMarket {pair.name}\nbuy price: {buy_price} - sell price: {sell_price} <> low: {low} - high: {high}\n')
 
-  # EXAMPLE ticker = MarketTicker(pair=Pair(name='ETH_BTC'),
+    # EXAMPLE ticker = MarketTicker(pair=Pair(name='ETH_BTC'),
     # buy_price=0.027638, sell_price=0.027641, trade_price=0.027641,
     # time=1608466157, volume=10596.948,
     # high=0.028549, low=0.027439, change=-0.001)
 
-  # Get my BTC balance
-  balances = await account.get_balance()
-  base_currency_balance = balances[cro_coin_base_currency]
-  base_currency_available = base_currency_balance.available
-  # EXAMPLE BTC_balance:Balance(total=0.04140678, available=3.243e-05, in_orders=0.04137435, in_stake=0, coin=Coin(name='BTC'))
+    # Get my BTC balance
+    balances = await account.get_balance()
+    base_currency_balance = balances[cro_coin_base_currency]
+    base_currency_available = base_currency_balance.available
+    # EXAMPLE BTC_balance:Balance(total=0.04140678, available=3.243e-05, in_orders=0.04137435, in_stake=0, coin=Coin(name='BTC'))
 
-  # Get my ETH balance
-  core_number_currency_balance = balances[cro_coin_core_number_currency]
+    # Get my ETH balance
+    core_number_currency_balance = balances[cro_coin_core_number_currency]
 
-  logger.info(f'Balances\n(Base) {base_currency} balance:{base_currency_balance} \n(Core) {core_number_currency} balance:{core_number_currency_balance}\n')
+    logger.info(f'Balances\n(Base) {base_currency} balance:{base_currency_balance} \n(Core) {core_number_currency} balance:{core_number_currency_balance}\n')
 
-  ###########################
-  # Core Number Adjustments #
-  ###########################
-  deviated_core_number = base_currency_available / buy_price
-  logger.info(f'Core number adjustments')
-  logger.info(f'Core number: {core_number} {core_number_currency}')
-  logger.info(f'Deviated Core number:{deviated_core_number:.6f} {core_number_currency}')
-  excess = round(deviated_core_number - core_number, config['max_decimals_buy'])
-  increase_percentage = excess * 100 / core_number
-  missing = round(core_number - deviated_core_number, config['max_decimals_sell'])
-  decrease_percentage = missing * 100 / core_number
+    ###########################
+    # Core Number Adjustments #
+    ###########################
+    deviated_core_number = base_currency_available / buy_price
+    logger.info(f'Core number adjustments')
+    logger.info(f'Core number: {core_number} {core_number_currency}')
+    logger.info(f'Deviated Core number:{deviated_core_number:.6f} {core_number_currency}')
+    excess = round(deviated_core_number - core_number, config['max_decimals_buy'])
+    increase_percentage = excess * 100 / core_number
+    missing = round(core_number - deviated_core_number, config['max_decimals_sell'])
+    decrease_percentage = missing * 100 / core_number
 
-  if coreNumberExploded(core_number, deviated_core_number):
-    logger.info(f'> Exploded {increase_percentage:.2f}%\nConsider updating CoreNumber to {deviated_core_number:.6f}')
+    if coreNumberExploded(core_number, deviated_core_number, config['max_core_number_increase_percentage']):
+      logger.info(f'> Exploded {increase_percentage:.2f}%\nConsider updating CoreNumber to {deviated_core_number:.6f}')
 
-  elif coreNumberIncreased(core_number, deviated_core_number):
-    logger.info(f'Increased {increase_percentage:.2f}% - excess of {excess:.6f} {core_number_currency} denominated in {base_currency}')
-    tx_result = round(excess * buy_price, config['max_decimals_buy'])
-    logger.info(f'\n\n>>> Selling: {tx_result:.6f} {base_currency} at {buy_price} to park an excess of {excess:.6f} {core_number_currency}\n')
-    # Sell excess BTC => Market Sell for ETH_BTC is denominated in BTC!!!
-    if (not config['safe_mode_on']):
-      await account.buy_market(pair, tx_result)
+    elif coreNumberIncreased(core_number, deviated_core_number, config['min_core_number_increase_percentage'], config['max_core_number_increase_percentage']):
+      logger.info(f'Increased {increase_percentage:.2f}% - excess of {excess:.6f} {core_number_currency} denominated in {base_currency}')
+      tx_result = round(excess * buy_price, config['max_decimals_buy'])
+      logger.info(f'\n\n>>> Selling: {tx_result:.6f} {base_currency} at {buy_price} to park an excess of {excess:.6f} {core_number_currency}\n')
+      # Sell excess BTC => Market Sell for ETH_BTC is denominated in BTC!!!
+      if (not config['safe_mode_on']):
+        await account.buy_market(pair, tx_result)
 
-  elif coreNumberDecreased(core_number, deviated_core_number):
-    logger.info(f'Decreased {decrease_percentage:.2f}% - missing {missing:.6f} {core_number_currency} denominated in {base_currency}')
-    tx_result = missing * sell_price
-    logger.info(f'\n\n>>> Buying: {tx_result:.6f} {base_currency} at {buy_price} taking {missing:.6f} {core_number_currency} from reserves\n')
-    # Buy missing BTC => Sell ETH
-    if (not config['safe_mode_on']):
-      await account.sell_market(pair, missing)
+    elif coreNumberDecreased(core_number, deviated_core_number, config['min_core_number_decrease_percentage'], config['max_core_number_decrease_percentage']):
+      logger.info(f'Decreased {decrease_percentage:.2f}% - missing {missing:.6f} {core_number_currency} denominated in {base_currency}')
+      tx_result = missing * sell_price
+      logger.info(f'\n\n>>> Buying: {tx_result:.6f} {base_currency} at {buy_price} taking {missing:.6f} {core_number_currency} from reserves\n')
+      # Buy missing BTC => Sell ETH
+      if (not config['safe_mode_on']):
+        await account.sell_market(pair, missing)
 
-  elif coreNumberPlummeted(core_number, deviated_core_number):
-    logger.info(f'> Plummeted {decrease_percentage:.2f}%\nConsider updating CoreNumber to {deviated_core_number:.6f}')
+    elif coreNumberPlummeted(core_number, deviated_core_number, config['max_core_number_decrease_percentage']):
+      logger.info(f'> Plummeted {decrease_percentage:.2f}%\nConsider updating CoreNumber to {deviated_core_number:.6f}')
 
-  else:
-    logger.info(f'> Price is rock-solid stable')
+    else:
+      logger.info(f'> Price is rock-solid stable')
 
-  # Update balances after adjusting to core number
-  balances = await account.get_balance()
-  logger.info(f'Final {base_currency} available:{balances[cro_coin_base_currency].available} - {core_number_currency} available:{balances[cro_coin_core_number_currency].available}\n')
+    # Update balances after adjusting to core number
+    balances = await account.get_balance()
+    logger.info(f'Final {base_currency} available:{balances[cro_coin_base_currency].available} - {core_number_currency} available:{balances[cro_coin_core_number_currency].available}\n')
 
-
+    if config['test_mode_on']:
+      await asyncio.sleep(1)
+      break
+    else:
+      # Wait given seconds until next poll
+      await asyncio.sleep(config['seconds_between_iterations'])
 
 
 
@@ -129,22 +135,22 @@ def check_config(data):
     sys.exit(1)
 
 
-def coreNumberIncreased(core_number, deviated_core_number):
-  min_core_number_increase = core_number * 1.03 # >3% increase
-  max_core_number_increase = core_number * 1.10 # <10% increase
+def coreNumberIncreased(core_number, deviated_core_number, min_core_number_increase_percentage, max_core_number_increase_percentage):
+  min_core_number_increase = core_number * (1 + (min_core_number_increase_percentage/100))
+  max_core_number_increase = core_number * (1 + (max_core_number_increase_percentage/100))
   return deviated_core_number >= min_core_number_increase and deviated_core_number <= max_core_number_increase
 
-def coreNumberExploded(core_number, deviated_core_number):
-  max_core_number_increase = core_number * 1.10 # >10% increase
+def coreNumberExploded(core_number, deviated_core_number, max_core_number_increase_percentage):
+  max_core_number_increase = core_number * (1 + (max_core_number_increase_percentage/100))
   return deviated_core_number > max_core_number_increase
 
-def coreNumberDecreased(core_number, deviated_core_number):
-  min_core_number_decrease = core_number * 0.97 # >3% decrease
-  max_core_number_decrease = core_number * 0.90 # <10% decrease
+def coreNumberDecreased(core_number, deviated_core_number, min_core_number_decrease_percentage, max_core_number_decrease_percentage):
+  min_core_number_decrease = core_number * (1 - (min_core_number_decrease_percentage/100))
+  max_core_number_decrease = core_number * (1 - (max_core_number_decrease_percentage/100))
   return deviated_core_number <= min_core_number_decrease and deviated_core_number >= max_core_number_decrease
 
-def coreNumberPlummeted(core_number, deviated_core_number):
-  max_core_number_decrease = core_number * 0.90 # >10% decrease
+def coreNumberPlummeted(core_number, deviated_core_number, max_core_number_decrease_percentage):
+  max_core_number_decrease = core_number * (1 - (max_core_number_decrease_percentage/100))
   return deviated_core_number < max_core_number_decrease
 
 def setupLogger(log_filename):
@@ -166,4 +172,11 @@ def setupLogger(log_filename):
 
 
 
-asyncio.get_event_loop().run_until_complete(main())
+loop = asyncio.get_event_loop()
+try:
+  loop.run_until_complete(main())
+except KeyboardInterrupt:
+  pass
+finally:
+  print("Stopping Corecito...")
+  loop.close()
